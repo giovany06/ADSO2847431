624914758ecbd9d79d391e7a20071114c892e28b
giovany toro valencia <giovany.gtv@gmail.com>
2025-04-15 20:24:02 -0500

    actualizando

--- 03-laravel/petsapp/petsapp/resources/views/components/auth-header.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-    'title',
-    'description',
-])
-
-<div class="flex w-full flex-col gap-2 text-center">
-    <h1 class="text-xl font-medium">{{ $title }}</h1>
-    <p class="text-center text-sm">{{ $description }}</p>
-</div>
--- 03-laravel/petsapp/petsapp/resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Authentication -->
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <a href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                     this.closest('form').submit();">
-        Log Out
-    </a>
-</form>
